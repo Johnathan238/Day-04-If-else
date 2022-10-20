6a49c97e14c385e4b1266f14516b421444d945a0
Johnathan Umana <jonathanumana86@gmail.com>
2022-10-20 02:03:10 -0400

    Added a switch statment for isWeekendOrWeekday

--- level2.js
+++ level2.js
@@ -32,25 +32,49 @@
 
 // allStudentsGrades("C")
 
-let whatSeason = prompt("What Season is it?:")
-switch(whatSeason){
-    case "Autumn":
-        whatSeason = "Autumn"
-        alert(` September, October or November, the season is ${whatSeason}.`)
-        break
+// let whatSeason = prompt("What Season is it?:")
+// switch(whatSeason){
+//     case "Autumn":
+//         whatSeason = "Autumn"
+//         alert(` September, October or November, the season is ${whatSeason}.`)
+//         break
 
-    case "Winter":
-        whatSeason = "Winter"
-        alert(`December, January or February, the season is ${whatSeason}.`)
-        break
+//     case "Winter":
+//         whatSeason = "Winter"
+//         alert(`December, January or February, the season is ${whatSeason}.`)
+//         break
 
-    case "Spring":
-        whatSeason = "Spring"
-        alert(`March, April or May, the season is ${whatSeason}.`)
-        break
+//     case "Spring":
+//         whatSeason = "Spring"
+//         alert(`March, April or May, the season is ${whatSeason}.`)
+//         break
 
-    case "Summer":
-        whatSeason = "Summer"
-        alert(`une, July or August, the season is ${whatSeason}.`)
-        break
-}
+//     case "Summer":
+//         whatSeason = "Summer"
+//         alert(`une, July or August, the season is ${whatSeason}.`)
+//         break
+// }
+
+
+let isWeekendOrWeekday = prompt("What day is today?")
+    switch(isWeekendOrWeekday){
+        case "Monday":
+            isWeekendOrWeekday = "Monday"
+            case "Tuesday":
+                isWeekendOrWeekday = "Tuesday"
+                case "Wednesday":
+                    isWeekendOrWeekday = "Wednesday"
+                    case "Thursday":
+                        isWeekendOrWeekday = "Thursday"
+                        case "Friday":
+                            isWeekendOrWeekday = "Friday"
+                            alert(`${isWeekendOrWeekday} is a weekday!`)
+                            break
+
+        case "Saturday":
+            isWeekendOrWeekday = "Saturday"
+            case "Sunday":
+                isWeekendOrWeekday = "Sunday"
+                alert(`${isWeekendOrWeekday} is a weekend!`)
+                break
+    }
